ae293785e48688e3fd16f704eb3f86c75d6f320a
Sakshii7777 <sakshistonape07@gmail.com>
2025-06-13 11:00:03 +0530

    created new CreateProduct File for FORK checking

---
+++ comcastCRMGUIFramework2/src/main/java/com/comcast/crm/objectrepositoryutility/CreateProduct.java
@@ -0,0 +1,9 @@
+package com.comcast.crm.objectrepositoryutility;
+
+public class CreateProduct {
+	/**
+	 * Here I am writing code
+	 * for CreateProduct File
+	 * for to check Fork Operation
+	 */
+}
